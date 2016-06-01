fae3f7ce84eb846705592b47c1e46793305fb3c8
humphrey <humphrey@parasoft.com>
2016-06-01 14:20:41 -0700

    @task94268 - start data repo as service

--- Artifacts/linux-data-repository-server/linux_install_data_repo.sh
+++ Artifacts/linux-data-repository-server/linux_install_data_repo.sh
@@ -17,7 +17,39 @@ elif [ -f /usr/bin/yum ] ; then
 fi
 echo "unziping DataRepository"
 unzip DataRepositoryServer.zip -d /opt/
+
+REPO_DIR=/opt/DataRepositoryServer-linux-x86_64
+
+groupadd datarepo
+useradd -M -s /bin/nologin -g datarepo -d /opt/DataRepositoryServer-linux-x86_64 datarepo
+chgrp -R datarepo $REPO_DIR/bin
+chgrp datarepo $REPO_DIR
+chmod g+rwx $REPO_DIR
+chown -R datarepo $REPO_DIR/bin/ $REPO_DIR/curl/
+
+if [ -f /usr/sbin/update-rc.d ] ; then
+    echo "Using Update-rc to register data repository as a service"
+
+    cp $REPO_DIR/server.sh /etc/init.d/
+    chmod 755 /etc/init.d/server.sh
+    update-rc.d server.sh defaults
+
+elif [ -f /bin/systemctl ] ; then
+    echo "Using Systemd to register data repository as a service"
+
+    cp repository.service /etc/systemd/system/repository.service
+    systemctl daemon-reload
+    systemctl enable repository
+
+fi
+
 echo "start Data Repository server"
-/opt/DataRepositoryServer-linux-x86_64/server.sh start
+if [ -f /usr/sbin/update-rc.d ] ; then
+    /etc/init.d/server.sh start
+elif [ -f /bin/systemctl ] ; then
+    systemctl start repository
+else
+    su - datarepo -c $REPO_DIR/server.sh start
+fi
 echo "cleanup"
 rm DataRepositoryServer.zip
