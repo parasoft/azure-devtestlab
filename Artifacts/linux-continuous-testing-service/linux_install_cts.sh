9eb06a16151d721977a3d1220f8d9eefd28f0c4e
humphrey <humphrey@parasoft.com>
2016-06-07 18:38:34 -0700

    @task94265 - install cts in it's own tomcat instance

--- Artifacts/linux-continuous-testing-service/linux_install_cts.sh
+++ Artifacts/linux-continuous-testing-service/linux_install_cts.sh
@@ -10,165 +10,191 @@
 #
 ###
 
+#cmd line argument that specifies the server name
 VIRTUALIZE_SERVER_NAME=$1
+
+#cmd line argument points to CTP server
 CTP_BASE_URL=$2
+
+#cmd line argument username used when trying to connect to CTP 
 CTP_USERNAME=$3
+
+#cmd line argument password used when trying to connect to CTP
 CTP_PASSWORD=$4
 
-# parse the URL in bash from http://stackoverflow.com/questions/6174220/parse-url-in-shell-script
-# extract the protocol
-proto="`echo $CTP_BASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
-# remove the protocol
-url=`echo $CTP_BASE_URL | sed -e s,$proto,,g`
-
-# extract the user and password (if any)
-userpass="`echo $url | grep @ | cut -d@ -f1`"
-pass=`echo $userpass | grep : | cut -d: -f2`
-if [ -n "$pass" ]; then
-    user=`echo $userpass | grep : | cut -d: -f1`
-else
-    user=$userpass
-fi
-
-# extract the host -- updated
-hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
-port=`echo $hostport | grep : | cut -d: -f2`
-if [ -n "$port" ]; then
-    host=`echo $hostport | grep : | cut -d: -f1`
-else
-    host=$hostport
-fi
-
-# extract the path (if any)
-path="`echo $url | grep / | cut -d/ -f2-`"
-
-CTP_HOST=$host
-
-echo "Installing Oracle JDK"
-echo "====================="
-
-if [ ! -d /usr/downloads ]; then
-   mkdir /usr/downloads
-fi
-
-if [ ! -f /usr/downloads/jdk-8u92-linux-x64.tar.gz ]; then
-   wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O /usr/downloads/jdk-8u92-linux-x64.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
-fi
-
-if [ -d /usr/oraclejdk ]; then
-   rm -rf /usr/oraclejdk
-fi
-mkdir /usr/oraclejdk
-tar -xvf /usr/downloads/jdk-8u92-linux-x64.tar.gz -C /usr/oraclejdk
-
+#JAVA_HOME points to the oracle java 8 binaries
 export JAVA_HOME=/usr/oraclejdk/jdk1.8.0_92
-echo "export JAVA_HOME=/usr/oraclejdk/jdk1.8.0_92" > /etc/profile.d/java.sh
 
-if type -p java; then
-   version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
-   echo $version
-fi
+#CATALINA_HOME points to the tomcat library files
+export CATALINA_HOME=/usr/local/tomcat
 
-if [[ "$version" = "1.8.0_92"  ]]; then
-   echo "Oracle JDK installation complete"
-else 
-   echo "Oracle JDK installation failed" 
-fi
+#CATALINA_BASE points to the CTS tomcat instance
+export CATALINA_BASE=/var/tomcat/cts
 
-echo "Installing Tomcat 8"
-echo "====================="
-
-echo "Downloading and unpacking tomcat 8 tar"
-TOMCAT_VERSION=8.0.35
-curl --silent --location --remote-name http://mirror.nexcess.net/apache/tomcat/tomcat-8/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
-tar xvzf apache-tomcat-$TOMCAT_VERSION.tar.gz
-mv apache-tomcat-$TOMCAT_VERSION /opt/tomcat
-echo "Tomcat 8 environment setup"
-export CATALINA_HOME=/opt/tomcat
-groupadd tomcat
-useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
-mkdir -p $CATALINA_HOME/conf/Catalina/localhost
-chgrp -R tomcat $CATALINA_HOME/conf
-chmod g+rwx $CATALINA_HOME/conf
-chmod g+r $CATALINA_HOME/conf/*
-chgrp tomcat $CATALINA_HOME
-chmod g+rwx $CATALINA_HOME
-chown -R tomcat $CATALINA_HOME/webapps/ $CATALINA_HOME/work/ $CATALINA_HOME/temp/ $CATALINA_HOME/logs/
-
-if [ -f /usr/sbin/update-rc.d ] ; then
-    echo "Using Update-rc to register Tomcat as a service"
-
-    cp tomcat.sh /etc/init.d/
-    chmod 755 /etc/init.d/tomcat.sh
-    update-rc.d tomcat.sh defaults
-
-elif [ -f /bin/systemctl ] ; then
-    echo "Using Systemd to register Tomcat as a service"
-
-    cp tomcat.service /etc/systemd/system/tomcat.service
-    systemctl daemon-reload
-    systemctl enable tomcat
-
-fi
-echo "cleanup"
-rm apache-tomcat-$TOMCAT_VERSION.tar.gz
-echo "Tomcat 8 installation finished"
-
-echo "Install zip and unzip"
-if [ -f /usr/bin/apt ] ; then
+init() {
+  echo "Install zip and unzip"
+  if [ -f /usr/bin/apt ] ; then
     echo "Using APT package manager"
 
     apt-get -y install zip unzip
 
-elif [ -f /usr/bin/yum ] ; then 
+  elif [ -f /usr/bin/yum ] ; then 
     echo "Using YUM package manager"
 
     yum clean all
 
     yum install -y zip unzip
-fi
+  fi
+}
+
+installJava() {
+  echo "Installing Oracle JDK"
+  echo "==============================================="
+  if [ -d /usr/oraclejdk ]; then
+    echo "Oracle JDK already installed"
+    return 0
+  fi
+  wget --quiet --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O jdk-8u92-linux-x64.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
+  mkdir /usr/oraclejdk
+  tar -xvf jdk-8u92-linux-x64.tar.gz -C /usr/oraclejdk
+  echo "export JAVA_HOME=/usr/oraclejdk/jdk1.8.0_92" > /etc/profile.d/java.sh
+  if type -p java; then
+   version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+   echo $version
+  fi
+  if [[ "$version" = "1.8.0_92"  ]]; then
+   echo "Oracle JDK installation complete"
+  else 
+   echo "Oracle JDK installation failed" 
+  fi
+  echo "remove jdk tar file"
+  rm jdk-8u92-linux-x64.tar.gz
+  echo "==============================================="
+}
+
+installTomcat() {
+  echo "Installing CTS Tomcat instance"
+  echo "==============================================="
+
+  TOMCAT_VERSION=8.0.35
+  if [ -d /usr/local/tomcat ]; then
+    echo "tomcat package already found in target directory"
+  else 
+    echo "Downloading and unpacking tomcat 8 tar"
+    curl --silent --location --remote-name http://mirror.nexcess.net/apache/tomcat/tomcat-8/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
+    tar xvzf apache-tomcat-$TOMCAT_VERSION.tar.gz
+    mv apache-tomcat-$TOMCAT_VERSION /usr/local/tomcat
+  fi
+  echo "creating CTS tomcat instance"
+  mkdir -p /var/tomcat/cts
+  echo "export CATALINA_BASE=/var/tomcat/cts" > /etc/profile.d/tomcat.sh
+  mkdir $CATALINA_BASE/conf
+  mkdir $CATALINA_BASE/logs
+  mkdir $CATALINA_BASE/temp
+  mkdir $CATALINA_BASE/webapps
+  mkdir $CATALINA_BASE/work
+  cp $CATALINA_HOME/conf/server.xml $CATALINA_BASE/conf/
+  cp $CATALINA_HOME/conf/web.xml $CATALINA_BASE/conf/
+  cp -r $CATALINA_HOME/webapps/* $CATALINA_BASE/webapps/
+  echo "configure CATALINA_HOME permissions"
+  groupadd parasoft
+  chgrp -R parasoft $CATALINA_HOME/bin
+  chmod g+rwx $CATALINA_HOME/bin
+  echo "configure CTS CATALINA_BASE permissions"
+  useradd -M -s /bin/nologin -g parasoft -d /var/tomcat/cts cts
+  mkdir -p $CATALINA_BASE/conf/Catalina/localhost
+  chgrp -R parasoft $CATALINA_BASE/conf
+  chmod g+rwx $CATALINA_BASE/conf
+  chmod g+r $CATALINA_BASE/conf/*
+  chgrp parasoft $CATALINA_BASE
+  chmod g+rwx $CATALINA_BASE
+  chown -R cts $CATALINA_BASE/webapps/ $CATALINA_BASE/work/ $CATALINA_BASE/temp/ $CATALINA_BASE/logs/
+
+  if [ -f /usr/sbin/update-rc.d ] ; then
+    echo "Using Update-rc to register CTS as a service"
+
+    cp cts.sh /etc/init.d/
+    chmod 755 /etc/init.d/cts.sh
+    update-rc.d cts.sh defaults
+
+  elif [ -f /bin/systemctl ] ; then
+    echo "Using Systemd to register CTS as a service"
+
+    cp cts.service /etc/systemd/system/cts.service
+    systemctl daemon-reload
+    systemctl enable cts
+  fi
+
+  if [ -f apache-tomcat-$TOMCAT_VERSION.tar.gz]; then
+    echo "remove tomcat 8 tar"
+    rm apache-tomcat-$TOMCAT_VERSION.tar.gz
+  fi
+  echo "==============================================="
+}
+
+installCTS() {
+  echo "Installing CTS"
+  echo "==============================================="
+  echo "Download CTS distribution"
+  curl --silent --location --remote-name http://parasoft.westus.cloudapp.azure.com/builds/parasoft_continuous_testing_service_9.9.5.war
+
+  echo "Extract CTS webapp"
+  VIRTUALIZE_HOME=/usr/local/parasoft/virtualize
+  mkdir -p $VIRTUALIZE_HOME
+  unzip parasoft_continuous_testing_service_9.9.5.war -d $VIRTUALIZE_HOME
+  rm parasoft_continuous_testing_service_9.9.5.war
+
+  echo "Configure Tomcat to deploy CTS webapp"
+  echo "<Context docBase=\"$VIRTUALIZE_HOME\" path=\"\" reloadable=\"true\" />" > $CATALINA_BASE/conf/Catalina/localhost/ROOT.xml
+  sed -i "s/8080/9080/g" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/8443/9443/g" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/8080/9080/g" $CATALINA_BASE/conf/server.xml
+  sed -i "s/8443/9443/g" $CATALINA_BASE/conf/server.xml
+  sed -i "s/8009/0/g" $CATALINA_BASE/conf/server.xml
+
+  sed -i "s/^#env.manager.server.name=.*/env.manager.server.name=$VIRTUALIZE_SERVER_NAME/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s!^#env.manager.server=.*!env.manager.server=$CTP_BASE_URL!" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#env.manager.username=.*/env.manager.username=$CTP_USERNAME/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#env.manager.password=.*/env.manager.password=$CTP_PASSWORD/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#env.manager.notify=.*/env.manager.notify=true/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#virtualize.license.use_network=.*/virtualize.license.use_network=true/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#virtualize.license.network.edition=.*/virtualize.license.network.edition=custom_edition/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#virtualize.license.custom_edition_features=.*/virtualize.license.custom_edition_features=Server, Validate, Message Packs, Unlimited Hits\/Day, 1 Million Hits\/Day, 500000 Hits\/Day, 100000 Hits\/Day, 50000 Hits\/Day, 25000 Hits\/Day, 10000 Hits\/Day/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#license.network.host=.*/license.network.host=$CTP_HOST/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+  sed -i "s/^#license.network.port=.*/license.network.port=2002/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+
+  chown -R cts /usr/local/parasoft/virtualize/
+  echo "==============================================="
+}
 
 
-echo "Download CTS distribution"
-curl --silent --location --remote-name http://parasoft.westus.cloudapp.azure.com/builds/parasoft_continuous_testing_service_9.9.5.war
+startTomcat() {
+  echo "Startup Tomcat"
+  echo "==============================================="
+  if [ -f /usr/sbin/update-rc.d ] ; then
+    /etc/init.d/cts.sh start
+  elif [ -f /bin/systemctl ] ; then
+    systemctl start cts
+  else
+    su - cts -c $CATALINA_HOME/bin/startup.sh
+  fi
+  echo "waiting for CTS startup"
+  curl --silent --location http://localhost:9080/
+  echo "CTS started"
+  echo "==============================================="
+}
 
-echo "Extract CTS webapp"
-VIRTUALIZE_HOME=/usr/local/parasoft/virtualize
-mkdir -p $VIRTUALIZE_HOME
-unzip parasoft_continuous_testing_service_9.9.5.war -d $VIRTUALIZE_HOME
-rm parasoft_continuous_testing_service_9.9.5.war
+#initalize download command utilities needed for CTS installation
+init
 
-echo "Configure Tomcat to deploy CTS webapp"
-echo "<Context docBase=\"$VIRTUALIZE_HOME\" path=\"\" reloadable=\"true\" />" > $CATALINA_HOME/conf/Catalina/localhost/ROOT.xml
-sed -i "s/8080/9080/g" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/8443/9443/g" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/8080/9080/g" $CATALINA_HOME/conf/server.xml
-sed -i "s/8443/9443/g" $CATALINA_HOME/conf/server.xml
-sed -i "s/8009/0/g" $CATALINA_HOME/conf/server.xml
+#install oracle java 8 if not installed 
+installJava
 
-sed -i "s/^#env.manager.server.name=.*/env.manager.server.name=$VIRTUALIZE_SERVER_NAME/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s!^#env.manager.server=.*!env.manager.server=$CTP_BASE_URL!" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#env.manager.username=.*/env.manager.username=$CTP_USERNAME/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#env.manager.password=.*/env.manager.password=$CTP_PASSWORD/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#env.manager.notify=.*/env.manager.notify=true/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#virtualize.license.use_network=.*/virtualize.license.use_network=true/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#virtualize.license.network.edition=.*/virtualize.license.network.edition=custom_edition/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#virtualize.license.custom_edition_features=.*/virtualize.license.custom_edition_features=Server, Validate, Message Packs, Unlimited Hits\/Day, 1 Million Hits\/Day, 500000 Hits\/Day, 100000 Hits\/Day, 50000 Hits\/Day, 25000 Hits\/Day, 10000 Hits\/Day/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#license.network.host=.*/license.network.host=$CTP_HOST/" $VIRTUALIZE_HOME/WEB-INF/config.properties
-sed -i "s/^#license.network.port=.*/license.network.port=2002/" $VIRTUALIZE_HOME/WEB-INF/config.properties
+#install tomcat 8 if not installed and create CTS tomcat instance
+installTomcat
 
-chown -R tomcat /usr/local/parasoft/virtualize/
+#download CTS zip file and install in tomcat instance
+installCTS
 
-echo "Startup Tomcat"
-
-if [ -f /usr/sbin/update-rc.d ] ; then
-    /etc/init.d/tomcat.sh start
-elif [ -f /bin/systemctl ] ; then
-    systemctl start tomcat
-else
-    su - tomcat -c $CATALINA_HOME/bin/startup.sh
-fi
-
-echo "waiting for tomcat startup"
-curl --silent --location http://localhost:9080/
-echo "tomcat started"
+#start tomcat service
+startTomcat
