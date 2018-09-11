3e61da5542ad6d168bc3a92e412f72a9397e5dcb
Matt Love <mlove@parasoft.com>
2018-09-11 13:56:37 -0700

    update Apache Tomcat from version 8.5.28 to 8.5.34

--- Artifacts/linux-continuous-testing-platform/linux_install_ctp.sh
+++ Artifacts/linux-continuous-testing-platform/linux_install_ctp.sh
@@ -56,7 +56,7 @@ installTomcat() {
   echo "Installing CTP Tomcat instance"
   echo "==============================================="
 
-  TOMCAT_VERSION=8.5.28
+  TOMCAT_VERSION=8.5.34
   if [ -d /usr/local/tomcat ]; then
     echo "tomcat package already found in target directory"
   else 
--- Artifacts/linux-continuous-testing-service/linux_install_cts.sh
+++ Artifacts/linux-continuous-testing-service/linux_install_cts.sh
@@ -104,7 +104,7 @@ installTomcat() {
   echo "Installing CTS Tomcat instance"
   echo "==============================================="
 
-  TOMCAT_VERSION=8.5.28
+  TOMCAT_VERSION=8.5.34
   if [ -d /usr/local/tomcat ]; then
     echo "tomcat package already found in target directory"
   else 
